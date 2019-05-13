09f36d699e558e5258901ef44e26096e1f911c06
ryo-takahashi <ryo.takahashi.work@gmail.com>
2019-05-13 22:46:26 +0900

    init

---
+++ Huruhuru/Huruhuru.h
@@ -0,0 +1,19 @@
+//
+//  Huruhuru.h
+//  Huruhuru
+//
+//  Created by ryo-takahashi on 2019/05/13.
+//  Copyright © 2019 k0uhashi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Huruhuru.
+FOUNDATION_EXPORT double HuruhuruVersionNumber;
+
+//! Project version string for Huruhuru.
+FOUNDATION_EXPORT const unsigned char HuruhuruVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Huruhuru/PublicHeader.h>
+
+
